35a93e98820671d4b44c5f67515bce71abaa5a93
Jeff Rose <rosejn@gmail.com>
2017-01-17 00:44:33 -0700

    Removed the global state and cleaned up API.

--- src/clj/think/peer/net.clj
+++ src/clj/think/peer/net.clj
@@ -3,12 +3,18 @@
     [taoensso.timbre :as log]
     [chord.http-kit :refer [with-channel]]
     [chord.format :as cf]
+    [hiccup.core :refer [html]]
+    [hiccup.page :refer [include-css include-js]]
+    [org.httpkit.server :as http-kit]
+    [ring.middleware.resource :refer [wrap-resource]]
+    [ring.middleware.file :refer [wrap-file]]
+    [bidi.ring :refer [make-handler]]
     [clojure.core.async :refer [<! >! go go-loop] :as async]
     [think.peer.api :as api])
   (:import [org.fressian.handlers WriteHandler ReadHandler]
            [org.fressian.impl ByteBufferInputStream BytesOutputStream]))
 
-(defonce clients* (atom {}))
+(def DEFAULT-PEER-PORT 4242)
 
 (defn mapply
   "Apply a map as keyword args to a function.
@@ -20,12 +26,12 @@
 
 (defn run-handler
   [handler req]
+  (log/debug "request: " req)
   (let [msg-args (:args req)
         n (count msg-args)
         arglists (:arglists (meta handler))
         arg-counts (map count arglists)
         ok? (some #(= n %) arg-counts)
-        _ (println "arglists: " arglists)
         optional? (first (filter #(= '& (first %)) arglists))]
     (cond
       ok?  (apply handler msg-args)
@@ -36,92 +42,195 @@
                       {})))))
 
 (defn event-handler
-  [client-id handlers req]
-  (if-let [handler (get-in handlers [:event (:event req)])]
+  [api req]
+  (if-let [handler (get-in api [:event (:event req)])]
     (run-handler handler req)
     (log/info "Unhandled event: " req)))
 
 (defn rpc-event-handler
-  [client-id handlers {:keys [chan id] :as req}]
+  [api {:keys [chan id] :as req}]
   (go
-    (if-let [handler (get-in handlers [:rpc (:fn req)])]
+    (if-let [handler (get-in api [:rpc (:fn req)])]
       (let [v (run-handler handler req)
             res (if (satisfies? clojure.core.async.impl.protocols/ReadPort v)
                   (<! v)
                   v)]
         (>! chan {:event :rpc-response :id id :result res}))
-      (log/info "Unhandled rpc-request: " req))))
+      (log/error "Unhandled rpc-request: " req))))
 
 (defn subscription-event-handler
-  [client-id handlers {:keys [chan id] :as req}]
+  [peers* peer-id api {:keys [chan id] :as req}]
   (go
-    (if-let [handler (get-in handlers [:subscription (:fn req)])]
-      (let [pub-chan (apply handler (:args req))]
+    (if-let [handler (get-in api [:subscription (:fn req)])]
+      (let [res (apply handler (:args req))
+            res (if (map? res) res {:chan res})
+            pub-chan (:chan res)]
         (if (satisfies? clojure.core.async.impl.protocols/ReadPort pub-chan)
-          (let [event-chan (async/chan 1 (map (fn [v] {:event :publication :id id :value v})))]
-            (async/pipe pub-chan event-chan)
-            (async/pipe event-chan chan)
-            (swap! clients* assoc-in [client-id :subscriptions id] pub-chan))
+          (let [event-wrapper (async/chan 1 (map (fn [v] {:event :publication :id id :value v})))]
+            (async/pipe pub-chan event-wrapper)
+            (async/pipe event-wrapper chan)
+            (swap! peers* assoc-in [peer-id :subscriptions id] res))
           (throw (ex-info (str "Subscription function didn't return a publication channel:" req)
                           {}))))
       (throw (ex-info (str "Unhandled subscription request: " req)
                       {})))))
 
 (defn unsubscription-event-handler
-  [client-id {:keys [chan id] :as req}]
-  (let [c (get-in @clients* [client-id :subscriptions id])]
-    (swap! clients* update-in [client-id :subscriptions] dissoc id)
-    (async/close! c)))
+  [peers* peer-id {:keys [id] :as req}]
+  (let [{:keys [chan stop]} (get-in @peers* [peer-id :subscriptions id])]
+    (swap! peers* update-in [peer-id :subscriptions] dissoc id)
+    (when (fn? stop)
+      (stop))
+    (async/close! chan)))
 
-(defn disconnect-client
-  [client-id]
-  (let [client (get @clients* client-id)]
-    (swap! clients* dissoc client-id)
-    (async/close! (:chan client))
-    (doseq [[sub-id sub-chan] (:subscriptions client)]
-      (async/close! sub-chan))))
+(defn disconnect-peer
+  [peers* peer-id]
+  (let [peer (get @peers* peer-id)]
+    (swap! peers* dissoc peer-id)
+    (async/close! (:chan peer))
+    (doseq [[sub-id {:keys [chan stop]}] (:subscriptions peer)]
+      (when (fn? stop)
+        (stop))
+      (async/close! chan))))
 
-(defn disconnect-all-clients
-  []
-  (doseq [client-id (keys @clients*)]
-    (disconnect-client client-id)))
+(defn disconnect-all-peers
+  [peers*]
+  (doseq [peer-id (keys @peers*)]
+    (disconnect-peer peers* peer-id)))
 
-(defn client-listener
-  "Setup a listener go-loop to receive messages from a client."
-  [handlers client-id client-chan]
-  (go-loop []
-    (let [{:keys [message error] :as packet} (<! client-chan)]
-      (if (or (nil? packet) error)
-        (do
-          (log/info "client disconnect")
-          (disconnect-client client-id))
-        (do
-          (when message
-            (log/info message)
-            (let [message (assoc message
-                                 :client-id client-id
-                                 :chan client-chan)
-                  event-type (:event message)]
-              (cond
-                (= event-type :rpc) (rpc-event-handler client-id handlers message)
-                (= event-type :subscription) (subscription-event-handler client-id handlers message)
-                (= event-type :unsubscription) (unsubscription-event-handler client-id message)
-                :default (event-handler client-id handlers message)))
-            (recur)))))))
+(defn- api-router
+  "Setup a router go-loop to handle received messages from a peer."
+  [{:keys [api* peers*] :as listener} peer-id]
+  (let [peer-chan (get-in @peers* [peer-id :chan])]
+    (go-loop []
+             (let [{:keys [message error] :as packet} (<! peer-chan)]
+               (if (or (nil? packet) error)
+                 (do
+                   (log/info "peer disconnect")
+                   (disconnect-peer peers* peer-id))
+                 (do
+                   (when message
+                     (log/info message)
+                     (let [message (assoc message
+                                          :peer-id peer-id
+                                          :chan peer-chan)
+                           event-type (:event message)]
+                       (cond
+                         (= event-type :rpc) (rpc-event-handler @api* message)
+                         (= event-type :subscription) (subscription-event-handler peers* peer-id @api* message)
+                         (= event-type :unsubscription) (unsubscription-event-handler peers* peer-id message)
+                         :default (event-handler @api* message)))
+                     (recur))))))))
 
-(defn connect-client
-  "Accepts a map of handlers and an http request.  The map holds hanlders in this shape:
+(defn connect-listener
+  "Connect an API listener to a websocket.  Takes an http-kit request
+  from a websocket endpoint.
 
-  {:event {'ping #'my.ns/ping}
-   :rpc {...}
-   :subscription {...}}"
-  [handlers req & [options]]
-  (with-channel req ws-ch {:format :transit-json}
+  (connect-listener listener request)
+  "
+  [{:keys [peers* api*] :as listener} req]
+  (with-channel req ws-ch {:format :edn}
     (go
       (let [{:keys [message error]} (<! ws-ch)]
         (if error
-          (log/warn "Client connect error:" error)
-          (let [client-id (:client-id message)]
-            (swap! clients* assoc client-id {:chan ws-ch :subscriptions {}})
-            (>! ws-ch {:type :connect-reply :success true})
-            (client-listener handlers client-id ws-ch)))))))
+          (log/warn "Peer connect error:" error)
+          (let [peer-id (:peer-id message)]
+            (swap! peers* assoc peer-id {:chan ws-ch :subscriptions {}})
+            (api-router listener peer-id)
+            (>! ws-ch {:type :connect-reply :success true})))))))
+
+
+(defn- page-template
+  [body]
+  {:status 200
+   :headers {}
+   :body (html
+          [:html
+           [:head
+            [:meta {:charset "utf-8"}]
+            [:meta {:name "viewport"
+                    :content "width=device-width, initial-scale=1"}]
+            (include-css "css/style.css")]
+           body])})
+
+(defn- app-page
+  [js css request]
+  (page-template
+    (apply conj
+      [:body
+       [:div#app
+        [:h3 "Clojurescript has not been compiled..."]]]
+      [:div (concat (map include-js js)
+                    (map include-css css))])))
+
+(defn- atom?
+  [x]
+  (instance? clojure.lang.Atom x))
+
+(defn peer-listener
+  "A peer listener holds the state need to serve an API to a set of one or more peers.
+  Requires an API config map.
+  * :api is an API map as described below
+
+  The API map contains three maps of handler functions:
+  * :event handlers are triggered when a peer fires an event
+  * :rpc the return value of RPC calls is sent back to the client
+  * :subscription subscription handlers should return a map with two keys
+    - :chan a channel that will be piped to the remote peer
+    - :stop an optional function to call on unsubscribe
+
+  (defn rand-eventer
+    []
+    (let [c (chan)]
+      (go
+        ; Using while to automatically exit the go form if the channel is closed
+        (while (>! c (rand-int 1000))
+          (<! (async/timeout 2000))
+
+  (peer-listener
+      :api {:event {'ping #'my.ns/ping}
+            :rpc {'add-two #(+ %1 %2)}
+            :subscription {'rand-val (fn [] {:chan (rand-eventer)})})
+  "
+  [api]
+  {:peers* (atom {})
+   :api* (if (atom? api) api (atom api))})
+
+
+;; Note:
+; If you have your own HTTP router (e.g. compojure or bidi) already setup to
+; receive websocket requests you can connect an API to each new socket using
+; the peer-listener and connect-listener function.
+;
+;  (listen listener request)
+;
+; Or you can use serve-api to serve the API on a port (listening for websocket
+; requests), optionally also serving files and a cljs app.
+
+(defn listen
+  "Serve an API on a specified port, listening for websocket connections.  The optional
+  :app-path argument is the path to a compiled cljs app which will be mounted into an
+  #app div when the root URL is requested.  The optional :ws-path determines the path used
+  to connect to the websocket."
+  [& {:keys [listener port ws-path js css] :as args}]
+  (log/debug (format "(listen %s)" args))
+  (let [port (or port DEFAULT-PEER-PORT)
+        ws-path (or ws-path "connect")
+        routes {ws-path (partial connect-listener listener)}
+        routes (if (or js css)
+                 (assoc routes "" (partial app-page js css) )
+                 routes)
+        app (-> (make-handler ["/" routes])
+                (wrap-resource "public")
+                (wrap-file "resources/public" {:allow-symlinks? true}))]
+    (log/info "routes: " routes)
+    (assoc listener
+           :port port
+           :server (http-kit/run-server app {:port port}))))
+
+(defn close
+  [{:keys [server peers*] :as s}]
+  (disconnect-all-peers peers*)
+  (when-let [server (:server s)]
+    (server)
+    (dissoc s :server)))
--- src/cljs/think/peer/net.cljs
+++ src/cljs/think/peer/net.cljs
@@ -1,41 +1,40 @@
 (ns think.peer.net
   (:require
+    [taoensso.timbre :as log]
     [chord.client :refer [ws-ch]]
-    [cljs.core.async :refer [<! >! put!] :as async])
+    [cljs.core.async :refer [<! >! put!] :as async]
+    [goog.string :as gstring]
+    [goog.string.format])
   (:require-macros
     [cljs.core.async.macros :refer [go go-loop]]))
+
+(def format gstring/format)
 
 (enable-console-print!)
 
 (def DEFAULT-HOST "localhost")
 (def DEFAULT-PORT 4242)
+(def DEFAULT-PATH "connect")
+
 (def RPC-TIMEOUT 3000)
 (def DISPATCH-BUFFER-SIZE 64) ; # of incoming msgs to buffer
 
-(def log println)
-
-(defonce CLIENT-ID (random-uuid))
-(defonce rpc-map* (atom {}))
-(defonce subscription-map* (atom {}))
-(defonce server-chan* (atom nil))
-(defonce event-chan* (atom nil))
-
-(defn setup-websocket
-  [url]
+(defn- websocket-chan
+  [url peer-id]
   (go
-    (let [conn (<! (ws-ch url {:format :transit-json}))
+    (let [conn (<! (ws-ch url {:format :edn}))
           {:keys [ws-channel error]} conn
-          _ (>! ws-channel {:type :connect :client-id CLIENT-ID})
+          _ (>! ws-channel {:type :connect :client-id peer-id})
           {:keys [message error]} (<! ws-channel)]
       (if error
         (do
-          (log "Error connecting to server:")
+          (log/error "Error connecting to host:")
           (js/console.log error))
         (do
-          (log "Connected to server!")
+          (log/info "Connected to host: " url)
           ws-channel)))))
 
-(defn dispatch-server-events
+(defn- dispatch-events
   [server-chan dispatch-chan]
   "Dispatch messages from server socket onto the event chan, where
   they can easily be subscribed to by :event type. "
@@ -43,50 +42,47 @@
     (let [packet (<! server-chan)
           {:keys [message error]} packet]
       (if error
-        (log "Websocket Error on server channel: " error)
+        (log/error "Websocket Error on server channel: " error)
         (do
-          ;(log "Server Event: " (:event message))
           (>! dispatch-chan message)
           (recur))))))
 
-(defn server-event-chan
-  "Given the websocket channel, returns a core.async/pub channel that can be
+(defn- pub-chan
+  "Takes a readable chan, returns a core.async/pub chan that can be
   subscribed to in order to receive server events of a specific type."
   [server-chan]
   (let [dispatch-chan (async/chan DISPATCH-BUFFER-SIZE)
         event-chan    (async/pub dispatch-chan :event)]
-    (dispatch-server-events server-chan dispatch-chan)
+    (dispatch-events server-chan dispatch-chan)
     event-chan))
 
-(defn disconnect-server
-  []
-  (put! @server-chan* {:event :disconnect})
-  (reset! server-chan* nil))
+(defn disconnect!
+  [{:keys [peer-chan] :as conn}]
+  (put! peer-chan {:event :disconnect})
+  (async/close! peer-chan))
 
-(defn send-event
-  [event & args]
+(defn event
+  [{:keys [peer-chan] :as conn} event & args]
   (let [e {:event event :args args :id (random-uuid)}]
-    (log "sending event: " e)
-    (put! @server-chan* e)))
+    (put! peer-chan e)))
 
-(defn subscribe-server-event
+(defn- event-type-chan
   "Returns a channel onto which all server events of a specific type will be placed."
-  [event-type & [buf]]
+  [event-chan event-type & [buf]]
   (let [c (if buf (async/chan buf) (async/chan))]
-    (async/sub @event-chan* event-type c)
+    (async/sub event-chan event-type c)
     c))
 
-(defn unsubscribe-server-event
-  "Unsubscribe a channel from receiving server events."
-  [event-type ch]
-  (async/unsub @event-chan* event-type ch))
+(defn- close-event-type-chan
+  "Unsubscribe an event type channel from receiving server events."
+  [event-chan event-type ch]
+  (async/unsub event-chan event-type ch))
 
-(defn handle-rpc-events
-  []
-  (let [rpc-events (subscribe-server-event :rpc-response)]
+(defn- handle-rpc-events
+  [event-chan rpc-map*]
+  (let [rpc-events (event-type-chan event-chan :rpc-response)]
     (go-loop []
       (let [{:keys [id] :as event} (<! rpc-events)]
-        ;; (log "rpc response: " event)
         (when-let [res-chan (get @rpc-map* id)]
           (>! res-chan event)
           (swap! rpc-map* dissoc id)))
@@ -95,56 +91,75 @@
 (defn request
   "Make an RPC request to the server.  Returns a channel that will receive the result, or nil on error.
   (The error will be logged to the console.)"
-  [fun & [args]]
+  [{:keys [rpc-map* peer-chan] :as conn} fun & [args]]
   (let [req-id (random-uuid)
         res-chan (async/chan)
         t-out (async/timeout RPC-TIMEOUT)
         event {:event :rpc :id req-id :fn fun :args (or args [])}]
-    (log "request:" event)
     (swap! rpc-map* assoc req-id res-chan)
     (go
-      (>! @server-chan* event)
+      (>! peer-chan event)
       (let [[v port] (alts! [res-chan t-out])]
         (cond
-          (= port t-out) (do ; Timeout
+          ; The request timed out
+          (= port t-out) (do
                            (swap! rpc-map* dissoc req-id)
                            (throw
                              (js/Error. (str "RPC Timeout: " fun))))
 
-          (and (= port res-chan) ; Got response
+          ; Got response
+          (and (= port res-chan)
                (:result v))      (:result v)
 
-          (and (= port res-chan) ; Got error
-               (:error v))       (log (str "RPC Error: " (:error v))))))))
+          ; Got error
+          (and (= port res-chan)
+               (:error v))       (log/error (str "RPC Error: " (:error v))))))))
 
-(defn connect-to-server
-  [url]
+; TODO: connect up the API so it works the same on the client as the server,
+; allowing the server to call functions and make subscriptions on the client.
+(defn connect
+  "Returns a peer connection that can be used to send events, make rpc requests, and
+  subscribe to peer event channels."
+  [& {:keys [url api host port path] :as args}]
   (go
-    (let [server-chan (<! (setup-websocket url))
-          event-chan  (server-event-chan server-chan)]
-      (reset! server-chan* server-chan)
-      (reset! event-chan* event-chan)
-      (handle-rpc-events))))
+    (let [id (random-uuid)
+          host (or host DEFAULT-HOST)
+          port (or port DEFAULT-PORT)
+          path (or path DEFAULT-PATH)
+          url (or url (format "ws://%s:%s/%s" host port path))
+          peer-chan (<! (websocket-chan url id))
+          ec (pub-chan peer-chan)
+          rpc-map* (atom {})]
+      (handle-rpc-events ec rpc-map*)
+      {:peer-chan peer-chan
+       :event-chan ec
+       :rpc-map* rpc-map*
+       :subscription-map* (atom {})
+       :api* (atom api)})))
 
-(defn subscribe-to
-  "Returns a channel that will receive the data for a given publication."
-  [subscription & args]
-  (let [flow-events (subscribe-server-event :publication (async/sliding-buffer 1))
+(defn subscribe
+  "Subscribe to a remote topic.  Returns a channel that can receive async messages
+  published to from the connection."
+  [{:keys [event-chan subscription-map* peer-chan] :as conn} topic & args]
+  (let [flow-events (event-type-chan event-chan :publication (async/sliding-buffer 1))
         id (random-uuid)
         publication-chan (async/chan 1 (filter #(= id (:id %))))
         value-chan (async/chan 1 (map :value))
-        event {:event :subscription :id id
-               :fn subscription :args (or args [])}]
-    (log "subscription: " event)
+        event {:event :subscription
+               :id id
+               :fn topic
+               :args (or args [])}]
     (swap! subscription-map* assoc value-chan id)
-    (put! @server-chan* event)
+    (put! peer-chan event)
     (async/pipe flow-events publication-chan)
     (async/pipe publication-chan value-chan)
     value-chan))
 
-(defn unsubscribe-from
-  [ch]
+(defn unsubscribe
+  "Unsubscribe from a remote topic channel for a connection."
+  [{:keys [subscription-map* peer-chan] :as conn} ch]
   (let [id (get subscription-map* ch)
         event {:event :unsubscription :id id}]
-    (swap! subscription-map* dissoc id)
-    (put! @server-chan* event)))
+    (put! peer-chan event)
+    (swap! subscription-map* dissoc id)))
+
