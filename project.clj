7cc39e314f5abdd38a694a57584e19ec4942ddcb
Charles Gruenwald <charles@thinktopic.com>
2017-03-24 17:45:21 -0600

    Version 0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.peer "0.2.5-SNAPSHOT"
+(defproject thinktopic/think.peer "0.2.5"
   :description "P2P - Clojure(Script) style"
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                  [org.clojure/clojurescript "1.9.293"]
