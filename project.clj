766a78d35ec4e8e62efb77be56603f7e741c3e3c
Charles Gruenwald <charles@thinktopic.com>
2017-03-24 12:17:49 -0600

    [project] Move release info to a release task with an alias. Update deps.

--- project.clj
+++ project.clj
@@ -2,37 +2,36 @@
   :description "P2P - Clojure(Script) style"
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                  [org.clojure/clojurescript "1.9.293"]
-                 [org.clojure/core.async "0.2.395"]
-                 [com.taoensso/timbre "4.7.4"]
-                 [http-kit "2.1.18"]
-                 [jarohen/chord "0.7.0" :exclusions [org.clojure/tools.reader]]
-                 [ring/ring-defaults "0.2.1"]
-                 [bidi "2.0.13"]
+                 [org.clojure/core.async "0.3.442"]
+                 [com.taoensso/timbre "4.8.0"]
+                 [http-kit "2.2.0"]
+                 [jarohen/chord "0.8.1" :exclusions [org.clojure/tools.reader]]
+                 [ring/ring-defaults "0.2.3"]
+                 [bidi "2.0.16"]
                  [stylefruits/gniazdo "1.0.0"]]
-
-  :plugins [[s3-wagon-private "1.3.0"]]
 
   :source-paths ["src/clj" "src/cljs"]
 
   :profiles {:test {:dependencies [[stylefruits/gniazdo "1.0.0"]
-                                   [com.cognitect/transit-clj "0.8.297"]]}}
+                                   [com.cognitect/transit-clj "0.8.300"]]}
+             :release {:plugins [[s3-wagon-private "1.3.0"]]
+                       :release-tasks [["vcs" "assert-committed"]
+                                       ["change" "version" "leiningen.release/bump-version" "release"]
+                                       ["vcs" "commit"]
+                                       ["vcs" "tag" "" "--no-sign"] ; disable signing
+                                       ["deploy"]
+                                       ["change" "version" "leiningen.release/bump-version"]
+                                       ["vcs" "commit"]
+                                       ["vcs" "push"]]
+                       :repositories  {"snapshots"  {:url "s3p://thinktopic.jars/snapshots/"
+                                                     :no-auth true
+                                                     :releases false
+                                                     :sign-releases false}
+                                       "releases"  {:url "s3p://thinktopic.jars/releases/"
+                                                    :no-auth true
+                                                    :snapshots false
+                                                    :sign-releases false}}}}
 
   :clean-targets ^{:protect false} ["resources/public/js/test/"]
 
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag" "" "--no-sign"] ; disable signing
-                  ["deploy"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]]
-
-  :repositories  {"snapshots"  {:url "s3p://thinktopic.jars/snapshots/"
-                                :no-auth true
-                                :releases false
-                                :sign-releases false}
-                  "releases"  {:url "s3p://thinktopic.jars/releases/"
-                               :no-auth true
-                               :snapshots false
-                               :sign-releases false}})
+  :aliases {"release" ["with-profile" "release" "release"]})
