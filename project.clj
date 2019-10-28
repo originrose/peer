393b0559249288174df6f209d952078a8721ab0f
Daniel Zurawski <djzurawski@gmail.com>
2019-10-28 16:03:30 -0600

    update peer to use latest transit-clj/transit-cljs

--- project.clj
+++ project.clj
@@ -1,16 +1,19 @@
 (defproject peer "0.4.0-SNAPSHOT"
   :description "P2P - Clojure(Script) style"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.516"]
                  [org.clojure/core.async "0.4.490"]
-                 [io.pedestal/pedestal.interceptor "0.5.5"]
+                 [io.pedestal/pedestal.interceptor "0.5.2"]
                  [com.taoensso/timbre "4.10.0"]
                  [http-kit "2.3.0"]
-                 [jarohen/chord "0.8.1" :exclusions [org.clojure/tools.reader]]
+                 [jarohen/chord "0.8.1" :exclusions [org.clojure/tools.reader
+                                                     com.cognitect/transit-cljs
+                                                     com.cognitect/transit-clj]]
                  [ring/ring-defaults "0.2.3"]
                  [bidi "2.0.16"]
                  [stylefruits/gniazdo "1.0.1"]
-                 [com.cognitect/transit-clj "0.8.300"]]
+                 [com.cognitect/transit-clj "0.8.300"]
+                 [com.cognitect/transit-cljs "0.8.256"]]
 
   :source-paths ["src/clj" "src/cljs"]
 
