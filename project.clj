5c09be4ed8f23742bb3dcd5b1febe09bab380ae5
Ahmed Almutawa <ahmed.almutawa@colorado.edu>
2020-04-13 09:27:03 -0600

    Private s3 wagon

--- project.clj
+++ project.clj
@@ -28,4 +28,13 @@
                   ["vcs" "commit"]
                   ["vcs" "push"]]
 
-  :clean-targets ^{:protect false} [:target-path "figwheel_server.log" "resources/public/js/test/"])
+  :clean-targets ^{:protect false} [:target-path "figwheel_server.log" "resources/public/js/test/"]
+  :repositories  {"snapshots" {:url "s3p://thinktopic.jars/snapshots/"
+                               :no-auth true
+                               :releases false
+                               :sign-releases false}
+                  "releases" {:url "s3p://thinktopic.jars/releases/"
+                              :no-auth true
+                              :snapshots false
+                              :sign-releases false}}
+  )
